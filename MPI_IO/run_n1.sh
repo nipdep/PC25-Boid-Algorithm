68f7e645c77ff237d5d47f437e021a4232eb3c7a
DevinDeSilva <devin.18@cse.mrt.ac.lk>
2025-04-16 14:29:10 -0400

    [update] print Algo name

--- MPI_IO/run_n1.sh
+++ MPI_IO/run_n1.sh
@@ -5,13 +5,14 @@ module load xl_r spectrum-mpi cuda/11.2
 STRONG_FLOCK_SIZE=( 100 1000 10000 100000)
 WEAK_FLOCK_SIZE=( 100 1000 10000 100000)
 THREAD_COUNT=(64 128 256 512 1024)
-TIME_STEP=30
+TIME_STEP=10
 NP_COUNT=( 1 2 4 )
+
 
 for k in "${STRONG_FLOCK_SIZE[@]}"; do
     for j in "${THREAD_COUNT[@]}"; do
         for i in "${NP_COUNT[@]}"; do
-            echo "|FILEOUT|MPI_IO|STRONG_SCALE|THREAD_COUNT $j|NP_COUNT $i|BOIDS_COUNT $k|"
+            echo "|FILEOUT|ALGO MPI_IO|STRONG_SCALE|THREAD_COUNT $j|NP_COUNT $i|BOIDS_COUNT $k|"
             mpirun --bind-to core --report-bindings -np "$i" ./strong_scale/mpi-cuda-exe $k $j $TIME_STEP 0
         done
         echo "Elapsed time: ${SECONDS} seconds"
@@ -21,7 +22,7 @@ done
 for k in "${WEAK_FLOCK_SIZE[@]}"; do
     for j in "${THREAD_COUNT[@]}"; do
         for i in "${NP_COUNT[@]}"; do
-            echo "|FILEOUT|MPI_IO|WEAK_SCALE|THREAD_COUNT $j|NP_COUNT $i|BOIDS_COUNT $k|"
+            echo "|FILEOUT|ALGO MPI_IO|WEAK_SCALE|THREAD_COUNT $j|NP_COUNT $i|BOIDS_COUNT $k|"
             mpirun --bind-to core --report-bindings -np "$i" ./weak_scale/mpi-cuda-exe $k $j $TIME_STEP 1
         done
 
